92e48874fe722bd5b66338f9096fa67366370cfb
CesarNML <26180499+cesarnml@users.noreply.github.com>
2026-06-29 00:29:39 +0700

    test(P13.06): SettingsTab customization case order [red]

--- apps/menubar/Tests/MenubarTests/SettingsTabModelTests.swift
+++ apps/menubar/Tests/MenubarTests/SettingsTabModelTests.swift
@@ -4,9 +4,16 @@ import XCTest
 
 final class SettingsTabModelTests: XCTestCase {
 
-	func testHasFiveTabsInOrder() {
+	func testHasSixTabsInOrder() {
 		let model = SettingsTabModel()
-		XCTAssertEqual(model.tabs, [.general, .pet, .rpg, .developer, .about])
+		XCTAssertEqual(model.tabs.count, 6)
+		XCTAssertEqual(model.tabs, [.general, .pet, .customization, .rpg, .developer, .about])
+	}
+
+	func testCustomizationTabIsAtIndexTwo() {
+		let tabs = SettingsTab.allCases
+		XCTAssertEqual(tabs.count, 6)
+		XCTAssertEqual(tabs[2], .customization, "customization must appear between pet and rpg")
 	}
 
 	func testDefaultSelectionIsGeneral() {
@@ -35,6 +42,7 @@ final class SettingsTabModelTests: XCTestCase {
 	func testTabTitles() {
 		XCTAssertEqual(SettingsTab.general.title, "General")
 		XCTAssertEqual(SettingsTab.pet.title, "Pet")
+		XCTAssertEqual(SettingsTab.customization.title, "Customization")
 		XCTAssertEqual(SettingsTab.rpg.title, "RPG")
 		XCTAssertEqual(SettingsTab.developer.title, "Developer")
 		XCTAssertEqual(SettingsTab.about.title, "About")
